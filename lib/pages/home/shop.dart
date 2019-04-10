87ca7dd718f8401d3625c5d36d5b0b4b8ae7edd0
Zvi <zvi@zektec.com>
2019-04-10 11:56:05 +0300

    added shop template

--- lib/pages/home/shop.dart
+++ lib/pages/home/shop.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:cup_and_soup/widgets/core/pageHeader.dart';
+import 'package:cup_and_soup/widgets/core/page.dart';
+import 'package:cup_and_soup/pages/item.dart';
+import 'package:cup_and_soup/widgets/shop/gridItem.dart';
 
 class ShopPage extends StatefulWidget {
   ShopPage({Key key}) : super(key: key);
@@ -9,16 +11,39 @@ class ShopPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<ShopPage> {
+  Map<String, Widget> _gridListItems = {
+    "Item 1": ItemPage(name: "item 1",),
+    "Item 2": ItemPage(name: "item 1",),
+    "Item 3": ItemPage(name: "item 1",),
+    "Item 4": ItemPage(name: "item 1",),
+    "Item 5": ItemPage(name: "item 1",),
+    "Item 6": ItemPage(name: "item 1",),
+    "Item 7": ItemPage(name: "item 1",),
+  };
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          PageHeaderWidget(
-            title: "shop",
-          ),
-          Center(child: Text("shop page")),
-        ],
+    return PageWidget(
+      title: "shop",
+      child: GridView.builder(
+        physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: _gridListItems.length,
+        itemBuilder: (context, index) {
+          return GridItemWidget(
+            text: _gridListItems.keys.toList()[index],
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        _gridListItems.values.toList()[index]),
+              );
+            },
+          );
+        },
       ),
     );
   }
