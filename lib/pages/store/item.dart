7c3c13a849f7777889002153c9aaf2252c96a5d2
Zvi <zvi@zektec.com>
2019-04-18 15:41:44 +0300

    basic item page design

--- lib/pages/store/item.dart
+++ lib/pages/store/item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tags/selectable_tags.dart';
 
 import 'package:cup_and_soup/widgets/store/actionSection.dart';
 import 'package:cup_and_soup/models/item.dart';
@@ -19,28 +20,92 @@ class ItemPage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  height: 250,
-                  child: Hero(
-                    tag: item.barcode,
-                    child: FadeInImage.assetNetwork(
-                      placeholder: 'assets/images/loading.png',
-                      image:
-                          'https://www.osem.co.il/tm-content/uploads/2014/12/instant_0016_manaHamaChickenTasteNoodles.png',
-                      fit: BoxFit.contain,
+                Stack(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        margin: EdgeInsets.all(36),
+                        height: 230,
+                        child: Hero(
+                          tag: item.barcode,
+                          child: FadeInImage.assetNetwork(
+                            placeholder: 'assets/images/loading.png',
+                            image:
+                                'https://www.osem.co.il/tm-content/uploads/2014/12/instant_0016_manaHamaChickenTasteNoodles.png',
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
                     ),
+                    Positioned(
+                      right: 60.0,
+                      top: 40.0,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 6, horizontal: 16),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: item.stock > 0
+                              ? Theme.of(context).primaryColor
+                              : Colors.grey,
+                        ),
+                        child: Text(
+                          item.stock > 0 ? "In Stcok" : "Out of stock",
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  item.name,
+                  style: TextStyle(
+                    fontFamily: "BrandFont",
+                    fontSize: 65,
                   ),
                 ),
-                Text(item.name),
-                Text(item.hechsherim),
-                Text(item.desc),
-                Text(item.tags),
+                Text(
+                  "Hechsher: " +
+                      (item.hechsherim.toString() ?? "Not kosher..") +
+                      ".",
+                  style: TextStyle(
+                    fontFamily: "PrimaryFont",
+                    color: Colors.black54,
+                    fontSize: 16,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment(-1, 0),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 43),
+                  child: Text(item.desc),
+                ),
+                SelectableTags(
+                  backgroundContainer: Theme.of(context).canvasColor,
+                  activeColor: Colors.black,
+                  color: Colors.black,
+                  textActiveColor: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).primaryColor,
+                  textStyle: Theme.of(context).textTheme.body1,
+                  tags: (item.tags.split(",")).map((tag) {
+                    return Tag(
+                      title: tag,
+                      active: false,
+                    );
+                  }).toList(),
+                  columns: 3,
+                  onPressed: (tag) => print(tag),
+                ),
+                SizedBox(height: 42),
               ],
             ),
           ),
         ),
       ),
-      bottomNavigationBar: ActionSectionWidget(barcode: item.barcode, price:item.price),
+      bottomNavigationBar:
+          ActionSectionWidget(barcode: item.barcode, price: item.price),
     );
   }
 }
